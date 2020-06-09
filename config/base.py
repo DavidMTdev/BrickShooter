4e1c952c10e914c557692a9616a6433415e761cd
David Teixeira <david.teixeira31@gmail.com>
2020-06-10 00:03:58 +0200

    Add Base

---
+++ config/base.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# create an engine
+Engine = create_engine('mysql+pymysql://root:@localhost/brickShooter', echo=False)
+
+# create a configured "Session" class
+Session = sessionmaker(bind=Engine)
+# create a configured "Base" class
+Base = declarative_base()
