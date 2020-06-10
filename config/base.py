2d92e912fd8090716389ed80c0a1238d17405d4a
David Teixeira <david.teixeira31@gmail.com>
2020-06-11 00:23:53 +0200

    Update Base

--- config/base.py
+++ config/base.py
@@ -3,9 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # create an engine
-Engine = create_engine('mysql+pymysql://root:@localhost/brickShooter', echo=False)
+Engine = create_engine('mysql+pymysql://root:@localhost/brickShooter', echo=True)
 
 # create a configured "Session" class
 Session = sessionmaker(bind=Engine)
+
+session = Session()
+
 # create a configured "Base" class
 Base = declarative_base()
